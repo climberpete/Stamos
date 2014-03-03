e2fb068a71f902a26b1dceedf96b8082de061cff
Peter <climberpete@gmail.com>
2014-03-02 22:05:42 -0700

    Images grouped by square, wide or tall.  Exclude images with ratio greater than 2:1 or less than 1:2

--- Source/content_script.js
+++ Source/content_script.js
@@ -1,39 +1,73 @@
-var library = imgLibrary();
-
-imgFind();
+var wide = 1;
+var square = 2;
+var tall = 3;
 
 function imgFind() {
     var imgs = document.getElementsByTagName("img");
     var imgSrcs = [];
 
     for (var i = 0; i < imgs.length; i++) {
-        console.log("image found: " + imgs[i].src);
-		console.log("img height: " + imgs[i].height);
-		console.log("img width: " + imgs[i].width);
-		var ratio = imgs[i].width/imgs[i].height;
-		var closest = null;
-		for (var j = 0; j < library.length; j++) {
-			if(closest == null || Math.abs(library[j].ratio - ratio) < Math.abs(closest.ratio - ratio)){
-				closest = library[j];
-			}
+		var img = imgs[i];
+		var ratio = img.width/img.height;
+		if(ratio > .5 && ratio < 2){
+			img.src = library.get(ratio).src
 		}
-		imgs[i].src = closest.src;
-		//imgs[i].style.background-repeat = 'repeat-y';
-		//imgs[i].style.width = '100%';
     }
 
     return imgSrcs;
 }
 
-function image(ratio, src){
+function Image(ratio, src){
 	this.ratio=ratio
 	this.src=src
 }
 
-function imgLibrary(){
-	var images = new Array()
-	images.push(new image(220/237, "http://upload.wikimedia.org/wikipedia/commons/thumb/d/df/JohnStamosHWoFFeb2012.jpg/220px-JohnStamosHWoFFeb2012.jpg"))
-	images.push(new image(320/240, "http://media2.s-nbcnews.com/j/streams/2013/August/130819/6C8670836-6c8284491-130716-john-stamos.blocks_desktop_small.jpg"))
-	images.push(new image(220/364, "http://blogs.babble.com/famecrawler/files/2010/11/45423PCN_Smile05.jpg"))
-	return images
+function Library(){
+	this.wide = [];
+	this.square = [];
+	this.tall = [];
 }
+
+Library.prototype.add = function(image){
+	if(image.ratio == 1) this.wide.push(image);
+	else if(image.ratio == 2) this.square.push(image);
+	else this.tall.push(image);
+};
+
+Library.prototype.get = function(ratio){
+	if(ratio > 1) {
+		return getRandom(this.wide);
+	}
+	if(ratio == 1) {
+		return getRandom(this.square);
+	}
+	if(ratio < 3) {
+		return getRandom(this.tall);
+	}
+}
+
+function buildLibrary(){
+	var library = new Library();
+	library.add(new Image(square, "http://upload.wikimedia.org/wikipedia/commons/thumb/d/df/JohnStamosHWoFFeb2012.jpg/220px-JohnStamosHWoFFeb2012.jpg"));
+	library.add(new Image(wide, "http://media2.s-nbcnews.com/j/streams/2013/August/130819/6C8670836-6c8284491-130716-john-stamos.blocks_desktop_small.jpg"));
+	library.add(new Image(tall, "http://blogs.babble.com/famecrawler/files/2010/11/45423PCN_Smile05.jpg"));
+	library.add(new Image(tall, "http://ia.media-imdb.com/images/M/MV5BMTQxOTI1MDcxNV5BMl5BanBnXkFtZTcwMjEwNDgxNw@@._V1_SY317_CR16,0,214,317_.jpg"));
+	library.add(new Image(tall, "http://images6.fanpop.com/image/photos/32800000/John-Stamos-2011-john-stamos-32853878-387-500.jpg"));
+	library.add(new Image(tall, "http://assets-s3.usmagazine.com/uploads/assets/articles/64632-john-stamos-to-reunite-with-full-house-band-jesse-and-the-rippers-on-jimmy-fallo/1374005728_109418050_john-stamos-467.jpg"));
+	library.add(new Image(square, "http://upload.wikimedia.org/wikipedia/en/c/c9/JohnStamos-ER.jpg"));
+	library.add(new Image(wide, "http://images6.fanpop.com/image/photos/32800000/John-Stamos-john-stamos-32853900-1024-768.png"));
+	library.add(new Image(wide, "http://i.huffpost.com/gen/1373206/thumbs/n-JOHN-STAMOS-large570.jpg?6"));
+	library.add(new Image(wide, "http://www.1015theeagle.com/stationimages/blogimages/John-Stamos-Ross-Schultz-5552134.jpg"));
+	library.add(new Image(square, "http://www.sitcomsonline.com/photopost/data/2298/Shirtless-John-Stamos.jpg"));
+	library.add(new Image(wide, "http://cdn01.cdn.justjared.com/wp-content/uploads/2008/02/stamos-shirtless/john-stamos-shirtless-03.jpg"));
+	library.add(new Image(tall, "http://singleblink.files.wordpress.com/2012/06/stamos.jpg"));
+	return library;
+}
+
+function getRandom(array){
+	return array[Math.floor(Math.random() * array.length)];
+}
+
+var library = buildLibrary();
+
+imgFind();
